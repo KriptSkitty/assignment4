c2a700936bf539f0dbcf512bb207c6e10dff733e
KriptSkitty <maxedout522@gmail.com>
2014-10-27 20:45:13 -0700

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,7 @@
+<?php
+	$out =  bin2hex(file_get_contents("p2 - research.exe"));
+	echo substr($out, 151584, 40);
+	$new = str_replace(substr($out, 151584, 40), sha1($argv[1]), $out);
+	$new_bin = hex2bin($new);
+	file_put_contents ( 'tester.exe', $new_bin );
+?>
